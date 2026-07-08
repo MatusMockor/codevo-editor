c0828abb6247b327c7b7dba0fc6730aea8046616
Matúš Močkor <matus.mockor@gmail.com>
2026-07-08 07:02:28 +0200

    Decouple framework target types from Laravel

--- src/application/usePhpFrameworkTargets.ts
+++ src/application/usePhpFrameworkTargets.ts
@@ -1,8 +1,30 @@
 import type { MutableRefObject } from "react";
+import type { EditorPosition } from "../domain/languageServerFeatures";
+import type { PhpFrameworkRouteDefinition } from "../domain/phpFrameworkProviders";
+import type { PhpLaravelGateAbilityDefinition } from "../domain/phpLaravelAuthorization";
+import type { PhpLaravelConfigTarget } from "../domain/phpLaravelConfig";
+import type { PhpLaravelEnvTarget } from "../domain/phpLaravelEnv";
+import type {
+  PhpLaravelMiddlewareAliasDefinition,
+} from "../domain/phpLaravelMiddleware";
+import type { PhpLaravelTranslationTarget } from "../domain/phpLaravelTranslations";
+import type { PhpLaravelViewTarget } from "../domain/phpLaravelViews";
 import type { FileEntry, TextSearchGateway } from "../domain/workspace";
+import type {
+  PhpLaravelAuthGuardTarget,
+  PhpLaravelBroadcastConnectionTarget,
+  PhpLaravelCacheStoreTarget,
+  PhpLaravelDatabaseConnectionTarget,
+  PhpLaravelLogChannelTarget,
+  PhpLaravelMailMailerTarget,
+  PhpLaravelPasswordBrokerTarget,
+  PhpLaravelQueueConnectionTarget,
+  PhpLaravelRedisConnectionTarget,
+  PhpLaravelStorageDiskTarget,
+} from "./phpLaravelConfigDerivedTargets";
 import type { PhpFrameworkIntelligence } from "./phpFrameworkIntelligence";
 import { phpLaravelFrameworkTargetCollectorAdapter } from "./phpLaravelFrameworkTargetAdapter";
-import type { LaravelTargets } from "./useLaravelTargets";
+import type { WorkspaceFileTarget } from "./phpWorkspaceTargetCollector";
 
 export interface PhpFrameworkTargetsDependencies {
   currentWorkspaceRootRef: MutableRefObject<string | null>;
@@ -16,38 +38,111 @@ export interface PhpFrameworkTargetsDependencies {
   frameworkIntelligence: PhpFrameworkIntelligence;
 }
 
+export type PhpFrameworkNamedRouteTarget =
+  WorkspaceFileTarget<PhpFrameworkRouteDefinition>;
+export type PhpFrameworkAuthorizationAbilityTarget =
+  WorkspaceFileTarget<PhpLaravelGateAbilityDefinition>;
+export type PhpFrameworkMiddlewareAliasTarget =
+  WorkspaceFileTarget<PhpLaravelMiddlewareAliasDefinition>;
+export type PhpFrameworkEnvironmentTarget = PhpLaravelEnvTarget;
+export type PhpFrameworkViewTarget = PhpLaravelViewTarget;
+export type PhpFrameworkConfigTarget = PhpLaravelConfigTarget;
+export type PhpFrameworkTranslationTarget = PhpLaravelTranslationTarget;
+export type PhpFrameworkAuthGuardTarget = PhpLaravelAuthGuardTarget;
+export type PhpFrameworkCacheStoreTarget = PhpLaravelCacheStoreTarget;
+export type PhpFrameworkDatabaseConnectionTarget =
+  PhpLaravelDatabaseConnectionTarget;
+export type PhpFrameworkBroadcastConnectionTarget =
+  PhpLaravelBroadcastConnectionTarget;
+export type PhpFrameworkQueueConnectionTarget = PhpLaravelQueueConnectionTarget;
+export type PhpFrameworkRedisConnectionTarget = PhpLaravelRedisConnectionTarget;
+export type PhpFrameworkMailMailerTarget = PhpLaravelMailMailerTarget;
+export type PhpFrameworkPasswordBrokerTarget = PhpLaravelPasswordBrokerTarget;
+export type PhpFrameworkLogChannelTarget = PhpLaravelLogChannelTarget;
+export type PhpFrameworkStorageDiskTarget = PhpLaravelStorageDiskTarget;
+
+export interface PhpFrameworkViewNavigationTarget
+  extends PhpFrameworkViewTarget {
+  position: EditorPosition;
+}
+
+type PhpFrameworkCurrentDocumentTargetCollector<Target> = (
+  currentSource: string,
+  currentPath: string,
+) => Promise<Target[]>;
+
+type PhpFrameworkWorkspaceTargetCollector<Target> = () => Promise<Target[]>;
+
+type PhpFrameworkTargetFinder<Target> = (
+  targetName: string,
+) => Promise<Target | null>;
+
 export interface PhpFrameworkTargets {
-  collectNamedRouteTargets: LaravelTargets["collectPhpLaravelNamedRouteTargets"];
-  collectAuthorizationAbilityTargets: LaravelTargets["collectPhpLaravelGateAbilityTargets"];
-  collectMiddlewareAliasTargets: LaravelTargets["collectPhpLaravelMiddlewareAliasTargets"];
-  collectEnvironmentTargets: LaravelTargets["collectPhpLaravelEnvTargets"];
-  collectViewTargets: LaravelTargets["collectPhpLaravelViewTargets"];
-  collectConfigTargets: LaravelTargets["collectPhpLaravelConfigTargets"];
-  collectTranslationTargets: LaravelTargets["collectPhpLaravelTranslationTargets"];
-  collectAuthGuardTargets: LaravelTargets["collectPhpLaravelAuthGuardTargets"];
-  collectCacheStoreTargets: LaravelTargets["collectPhpLaravelCacheStoreTargets"];
-  collectDatabaseConnectionTargets: LaravelTargets["collectPhpLaravelDatabaseConnectionTargets"];
-  collectBroadcastConnectionTargets: LaravelTargets["collectPhpLaravelBroadcastConnectionTargets"];
-  collectQueueConnectionTargets: LaravelTargets["collectPhpLaravelQueueConnectionTargets"];
-  collectRedisConnectionTargets: LaravelTargets["collectPhpLaravelRedisConnectionTargets"];
-  collectMailMailerTargets: LaravelTargets["collectPhpLaravelMailMailerTargets"];
-  collectPasswordBrokerTargets: LaravelTargets["collectPhpLaravelPasswordBrokerTargets"];
-  collectLogChannelTargets: LaravelTargets["collectPhpLaravelLogChannelTargets"];
-  collectStorageDiskTargets: LaravelTargets["collectPhpLaravelStorageDiskTargets"];
-  findViewTarget: LaravelTargets["findPhpLaravelViewTarget"];
-  findConfigTarget: LaravelTargets["findPhpLaravelConfigTarget"];
-  findTranslationTarget: LaravelTargets["findPhpLaravelTranslationTarget"];
-  findAuthGuardTarget: LaravelTargets["findPhpLaravelAuthGuardTarget"];
-  findCacheStoreTarget: LaravelTargets["findPhpLaravelCacheStoreTarget"];
-  findDatabaseConnectionTarget: LaravelTargets["findPhpLaravelDatabaseConnectionTarget"];
-  findBroadcastConnectionTarget: LaravelTargets["findPhpLaravelBroadcastConnectionTarget"];
-  findQueueConnectionTarget: LaravelTargets["findPhpLaravelQueueConnectionTarget"];
-  findRedisConnectionTarget: LaravelTargets["findPhpLaravelRedisConnectionTarget"];
-  findMailMailerTarget: LaravelTargets["findPhpLaravelMailMailerTarget"];
-  findPasswordBrokerTarget: LaravelTargets["findPhpLaravelPasswordBrokerTarget"];
-  findLogChannelTarget: LaravelTargets["findPhpLaravelLogChannelTarget"];
-  findStorageDiskTarget: LaravelTargets["findPhpLaravelStorageDiskTarget"];
-  invalidateTargetCache: LaravelTargets["invalidatePhpLaravelTargetCache"];
+  collectNamedRouteTargets: PhpFrameworkCurrentDocumentTargetCollector<
+    PhpFrameworkNamedRouteTarget
+  >;
+  collectAuthorizationAbilityTargets: PhpFrameworkCurrentDocumentTargetCollector<
+    PhpFrameworkAuthorizationAbilityTarget
+  >;
+  collectMiddlewareAliasTargets: PhpFrameworkCurrentDocumentTargetCollector<
+    PhpFrameworkMiddlewareAliasTarget
+  >;
+  collectEnvironmentTargets: PhpFrameworkWorkspaceTargetCollector<
+    PhpFrameworkEnvironmentTarget
+  >;
+  collectViewTargets: PhpFrameworkWorkspaceTargetCollector<PhpFrameworkViewTarget>;
+  collectConfigTargets: PhpFrameworkWorkspaceTargetCollector<PhpFrameworkConfigTarget>;
+  collectTranslationTargets: PhpFrameworkWorkspaceTargetCollector<
+    PhpFrameworkTranslationTarget
+  >;
+  collectAuthGuardTargets: PhpFrameworkWorkspaceTargetCollector<
+    PhpFrameworkAuthGuardTarget
+  >;
+  collectCacheStoreTargets: PhpFrameworkWorkspaceTargetCollector<
+    PhpFrameworkCacheStoreTarget
+  >;
+  collectDatabaseConnectionTargets: PhpFrameworkWorkspaceTargetCollector<
+    PhpFrameworkDatabaseConnectionTarget
+  >;
+  collectBroadcastConnectionTargets: PhpFrameworkWorkspaceTargetCollector<
+    PhpFrameworkBroadcastConnectionTarget
+  >;
+  collectQueueConnectionTargets: PhpFrameworkWorkspaceTargetCollector<
+    PhpFrameworkQueueConnectionTarget
+  >;
+  collectRedisConnectionTargets: PhpFrameworkWorkspaceTargetCollector<
+    PhpFrameworkRedisConnectionTarget
+  >;
+  collectMailMailerTargets: PhpFrameworkWorkspaceTargetCollector<
+    PhpFrameworkMailMailerTarget
+  >;
+  collectPasswordBrokerTargets: PhpFrameworkWorkspaceTargetCollector<
+    PhpFrameworkPasswordBrokerTarget
+  >;
+  collectLogChannelTargets: PhpFrameworkWorkspaceTargetCollector<
+    PhpFrameworkLogChannelTarget
+  >;
+  collectStorageDiskTargets: PhpFrameworkWorkspaceTargetCollector<
+    PhpFrameworkStorageDiskTarget
+  >;
+  findViewTarget: PhpFrameworkTargetFinder<PhpFrameworkViewNavigationTarget>;
+  findConfigTarget: PhpFrameworkTargetFinder<PhpFrameworkConfigTarget>;
+  findTranslationTarget: PhpFrameworkTargetFinder<PhpFrameworkTranslationTarget>;
+  findAuthGuardTarget: PhpFrameworkTargetFinder<PhpFrameworkAuthGuardTarget>;
+  findCacheStoreTarget: PhpFrameworkTargetFinder<PhpFrameworkCacheStoreTarget>;
+  findDatabaseConnectionTarget: PhpFrameworkTargetFinder<
+    PhpFrameworkDatabaseConnectionTarget
+  >;
+  findBroadcastConnectionTarget: PhpFrameworkTargetFinder<
+    PhpFrameworkBroadcastConnectionTarget
+  >;
+  findQueueConnectionTarget: PhpFrameworkTargetFinder<PhpFrameworkQueueConnectionTarget>;
+  findRedisConnectionTarget: PhpFrameworkTargetFinder<PhpFrameworkRedisConnectionTarget>;
+  findMailMailerTarget: PhpFrameworkTargetFinder<PhpFrameworkMailMailerTarget>;
+  findPasswordBrokerTarget: PhpFrameworkTargetFinder<PhpFrameworkPasswordBrokerTarget>;
+  findLogChannelTarget: PhpFrameworkTargetFinder<PhpFrameworkLogChannelTarget>;
+  findStorageDiskTarget: PhpFrameworkTargetFinder<PhpFrameworkStorageDiskTarget>;
+  invalidateTargetCache: () => void;
 }
 
 export interface PhpFrameworkTargetCollectorAdapter {
